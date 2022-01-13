b48458b9f6b0e5af2aee2b22a751493ccf8af3ab
mattgr99 <jmgranja1@espe.edu.ec>
2022-01-12 20:20:21 -0500

    Calculo EAR

--- DemoFaceDetection/app/src/main/java/com/vision/sample/ui/Graphics.java
+++ DemoFaceDetection/app/src/main/java/com/vision/sample/ui/Graphics.java
@@ -95,14 +95,32 @@ class Graphics extends CameraOverlay.Graphic {
             return;
         }
 
+        
         // Draws a circle at the position of the detected face, with the face's track id below.
         float x = translateX(face.getPosition().x + face.getWidth() / 2);
         float y = translateY(face.getPosition().y + face.getHeight() / 2);
         canvas.drawCircle(x, y, FACE_POSITION_RADIUS, mFacePositionPaint);
-        
-	/****
-	*
-	*/
+        //canvas.drawText("id: " + mFaceId, x + ID_X_OFFSET, y + ID_Y_OFFSET, mIdPaint);
+        //canvas.drawText("happiness: " + String.format("%.2f", face.getIsSmilingProbability()), x - ID_X_OFFSET, y - ID_Y_OFFSET, mIdPaint);
+        canvas.drawText("right eye: " + String.format("%.2f", face.getIsRightEyeOpenProbability()),
+                x + ID_X_OFFSET * 2,
+                y + ID_Y_OFFSET * 2, mIdPaint);
+        canvas.drawText("left eye: " + String.format("%.2f", face.getIsLeftEyeOpenProbability()),
+                x - ID_X_OFFSET*2,
+                y - ID_Y_OFFSET*2, mIdPaint);
+
+
+        //calculate EAR(Eye Aspect Radio)
+        double EAR = (face.getIsLeftEyeOpenProbability() + face.getIsRightEyeOpenProbability())/2;
+        EAR = Math.round(EAR*100.0)/100.0;
+        if (EAR<0.26){
+            canvas.drawText("SOMNOLIENTO ",
+                    200,
+                    1175, mIdPaint);
+            canvas.drawText("Deberias descansar " ,
+                    200,
+                    1275, mIdPaint);
+        }
 
         // Draws a bounding box around the face.
         float xOffset = scaleX(face.getWidth() / 2.0f);
